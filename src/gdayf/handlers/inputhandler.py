0c42ce2642c77a31bbbd192b1ccd230628a0391b
Luis Sanchez Bejarano <luis.sanchez.bejarano@gmail.com>
2017-05-26 13:07:31 +0000

    Creada las clases base del inputhandler

--- src/gdayf/handlers/inputhandler.py
+++ src/gdayf/handlers/inputhandler.py
@@ -1,9 +1,41 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import json
 
-class imputHandler:
+
+class inputHandler:
     def __init__(self):
-        False
+        True
 
-    
+
+class inputHandlerCSVLocal (inputHandler):
+    def __init__(self):
+        self.loadCSVDefaultParameters()
+
+    def loadCSVDefaultParameters(self):
+        self.sep = ','
+        self.delimiter = None
+        self.header = 'infer'
+        self.index_col = None
+
+    def inputCSV(self, filename=None, **kwargs):
+        return pd.read_csv(filename)
+
+    def createDataFramePandasMetadata(self, dataframe):
+        md = {}
+        md['type'] = 'pandas'
+        md['rowcount'] = dataframe.shape[0] - 1
+        md['cols'] = dataframe.shape[1]
+        md['timeformat'] = ''
+        md['columns'] = {}
+
+        return md
+
+
+if __name__ == "__main__":
+    ih = inputHandlerCSVLocal()
+    datos = ih.inputCSV('/home/luis/FL_insurance_sample.csv')
+    print("Tipos de datos: %s" % datos.dtypes)
+    metadatos = ih.createDataFramePandasMetadata(datos)
+    print("Metadatos: %s" % metadatos)
