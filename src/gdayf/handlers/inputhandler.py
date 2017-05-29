9f246524d208448703a0056c2f37241ccbfe2c36
Luis Sanchez Bejarano <luis.sanchez.bejarano@gmail.com>
2017-05-29 08:55:00 +0000

    Merged 0.0.2-fbretones into 0.0.2-lsanchez (quantiles, std)

--- src/gdayf/handlers/inputhandler.py
+++ src/gdayf/handlers/inputhandler.py
@@ -14,25 +14,21 @@ class inputHandler:
         md['type'] = '%s' % typedf
         md['rowcount'] = dataframe.shape[0] - 1
         md['cols'] = dataframe.shape[1]
-        md['timeformat'] = ''
+        md['timeformat'] = 'dd-mm-yyyy HH:mm:ss:ms'
         columnlist = []
         for col in dataframe.columns:
             summary = dataframe[col].describe()
             auxdict = OrderedDict()
             auxdict['name'] = dataframe[col].name
             auxdict['type'] = str(dataframe[col].dtype)
-            for comp in ['min', 'max', 'mean']:
+            for comp in ['min', 'max', 'mean', 'std', '25%', '50%', '75%']:
                 try:
                     auxdict[comp] = str(summary[comp])
                 except KeyError:
-                    auxdict[comp] = 'Nan'
-            auxdict['sigma'] = ''
+                    auxdict[comp] = 'NaN'
             auxdict['zeros'] = str(dataframe[col][dataframe[col] == 0].count())
             auxdict['missed'] = str(
                 dataframe[col].isnull().values.ravel().sum())
-            auxdict['firstq'] = ''
-            auxdict['secondq'] = ''
-            auxdict['thirdq'] = ''
             columnlist.append(auxdict)
         md['columns'] = columnlist
         return json.dumps(md, indent=4)
