b1533e3376f7ce346afe524b6a937ed988bc8ff3
Luis Sanchez Bejarano <luis.sanchez.bejarano@gmail.com>
2017-06-02 11:04:59 +0000

    Created new file dfmetada.py in common, this file contain method to create metada json file from dataframe panda, this method was included in inputhandler before but is better include it in common for use from other modules.

--- src/gdayf/handlers/inputhandler.py
+++ src/gdayf/handlers/inputhandler.py
@@ -3,35 +3,12 @@
 import pandas as pd
 import json
 from collections import OrderedDict
+from gdayf.common.dfmetada import DFMetada
 
 
 class inputHandler:
     def __init__(self):
         True
-
-    def getDataFrameMetadata(self, dataframe, typedf):
-        md = OrderedDict()
-        md['type'] = '%s' % typedf
-        md['rowcount'] = dataframe.shape[0] - 1
-        md['cols'] = dataframe.shape[1]
-        md['timeformat'] = 'dd-mm-yyyy HH:mm:ss:ms'
-        columnlist = []
-        for col in dataframe.columns:
-            summary = dataframe[col].describe()
-            auxdict = OrderedDict()
-            auxdict['name'] = dataframe[col].name
-            auxdict['type'] = str(dataframe[col].dtype)
-            for comp in ['min', 'max', 'mean', 'std', '25%', '50%', '75%']:
-                try:
-                    auxdict[comp] = str(summary[comp])
-                except KeyError:
-                    auxdict[comp] = 'NaN'
-            auxdict['zeros'] = str(dataframe[col][dataframe[col] == 0].count())
-            auxdict['missed'] = str(
-                dataframe[col].isnull().values.ravel().sum())
-            columnlist.append(auxdict)
-        md['columns'] = columnlist
-        return json.dumps(md, indent=4)
 
 
 class inputHandlerCSV (inputHandler):
@@ -41,6 +18,9 @@ class inputHandlerCSV (inputHandler):
     def inputCSV(self, filename=None, **kwargs):
         return pd.read_csv(filename, **kwargs)
 
+    def getCSVMetadata(self, dataframe, typedef):
+        return DFMetada.getDataFrameMetadata(dataframe, typedef)
+
 
 if __name__ == "__main__":
     ih = inputHandlerCSV()
@@ -48,7 +28,7 @@ if __name__ == "__main__":
 # Pruebas para cargar un fichero local csv
     datos = ih.inputCSV(
         '/home/luis/desarrollo/pruebas/FL_insurance_sample.csv')
-    metadatas = ih.getDataFrameMetadata(datos, 'pandas')
+    metadatas = ih.getCSVMetadata(datos, 'pandas')
     file = open('salida_local.json', 'w')
     file.writelines(metadatas)
     file.close()
@@ -62,7 +42,7 @@ if __name__ == "__main__":
         usecols=[0, 1, 2, 3, 4, 5],
         index_col=[0],
         encoding='iso-8859-1')
-    metadatas = ih.getDataFrameMetadata(datos, 'pandas')
+    metadatas = ih.getCSVMetadata(datos, 'pandas')
     file = open('salida_local_aeropuerto.json', 'w')
     file.writelines(metadatas)
     file.close()
@@ -70,7 +50,7 @@ if __name__ == "__main__":
 # Pruebas para cargar un fichero csv remoto mediante su url
     datos = ih.inputCSV(
         'http://catalogo.sevilla.org/dataset/621613fb-8315-4c00-915e-5ff0538f92c2/resource/21923b17-2c88-4b1f-9e50-b55cf2bbef6a/download/estadoejecuciongastos1semestregmu.csv')
-    metadatas = ih.getDataFrameMetadata(datos, 'pandas')
+    metadatas = ih.getCSVMetadata(datos, 'pandas')
     file = open('salida_remota.json', 'w')
     file.writelines(metadatas)
     file.close()
