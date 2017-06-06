6a67d281072912591cf40130cf9a0b1ebad27322
Luis Sanchez Bejarano <luis.sanchez.bejarano@gmail.com>
2017-06-06 10:35:17 +0000

    normalizer.py update

--- src/gdayf/normalizer/normalizer.py
+++ src/gdayf/normalizer/normalizer.py
@@ -10,45 +10,41 @@ class Normalizer:
         if (normalizemd['type'] == 'pandas'):
             for col in normalizemd['columns']:
                 if (col['class'] == 'mean'):
-                    self.normalizeMean(dataframe,
-                                       col['name'],
+                    dataframe[col['name']] = self.normalizeMean(dataframe[col['name']],
                                        col['objective']['mean'],
                                        col['objective']['std'])
                 elif (col['class'] == 'working_range'):
-                    self.normalizeWorkingRange(dataframe,
-                                               col['name'],
+                    dataframe[col['name']] = self.normalizeWorkingRange(dataframe[col['name']],
                                                col['objective']['minval'],
                                                col['objective']['maxval'])
                 elif (col['class'] == 'discretize'):
-                    self.normalizeDiscretize(dataframe,
-                                             col['name'],
-                                             col['objective']['buckets_number'],
-                                             col['objective']['fixed_size'])
+                    dataframe[col['name']] = self.normalizeDiscretize(dataframe[col['name']],
+                                                                      col['objective']['buckets_number'],
+                                                                      col['objective']['fixed_size'])
                 elif (col['class'] == 'agregation'):
-                    self.normalizeAgregation(dataframe,
-                                             col['name'],
+                    dataframe[col['name']] = self.normalizeAgregation(dataframe[col['name']],
                                              col['objective']['bucket_radio'])
                 elif (col['class'] == 'missing_values'):
-                    self.normalizeMissingValues(dataframe,
-                                                col['name'],
+                    dataframe[col['name']] = self.normalizeMissingValues(dataframe[col['name']],
                                                 col['objective']['mean'],
                                                 col['objective']['mode'],
                                                 col['objective']['fixed'],
                                                 col['objective']['k_nearest'],
                                                 col['objective']['value'])
                 elif (col['class'] == 'binary_encoding'):
-                    self.normalizeBinaryEncoding(dataframe,
-                                                 col['name'],
+                    dataframe[col['name']] = self.normalizeBinaryEncoding(dataframe[col['name']],
                                                  col['objective']['minval'],
                                                  col['objective']['maxval'])
                 else:
                     print("Nothing to Normalize")
 
-    def normalizeMean(self, dataframe, col, mean=0, std=1):
+                return dataframe
+
+    def normalizeMean(self, dataframe, mean=0, std=1):
         True
 
-    def normalizeWorkingRange(self, dataframe, col, minval=0, maxval=1):
+    def normalizeWorkingRange(self, dataframe, minval=0, maxval=1):
         assert(maxval > minval)
-        if (dataframe[col].dtype != numpy.object):
-            dataframe[col] = (maxval - minval) * ((dataframe[col] - dataframe[col].min()) / (dataframe[col].max() - dataframe[col].min())) + maxval
+        if (dataframe.dtype != numpy.object):
+            dataframe = (maxval - minval) * ((dataframe - dataframe.min()) / (dataframe.max() - dataframe.min())) + maxval
         return dataframe
