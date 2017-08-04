68cbacf3c28b1deb546d88754cd383b87e3450ce
Jose L. Sanchez del Coso <e2itses@gmail.com>
2017-08-04 14:34:54 +0000

    Executable beta state branch 0.1.0-mrazul 04/08/2017 16:32 Updated gDayF core version 0.1.0 Executed: Improvements on dataframe histograms including bindings Base beta DayF SaaS Bussines Case and Template of Financial Plan
    Sprint 3 (31/08) 4 weeks
    Include AutoEncoders Analysis (unsupervised)
    Include K-means Clustering Analysis (unsupervised)
    Optional: Include Naive-Bayes Analysis (supervised)
    Milestone: 24/08
    DayF SaaS Bussines Case and Financial Plan
    
    
    Pending Sprint Task backlog:)
        Confident thresholds on predictions (Non prioritary)
        Advanced analysis over one selected model (checkpoint param) (Non prioritary)

--- test/src/test_main-multinomial.py
+++ test/src/test_main-multinomial.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     #Analysis
     controller = Controller()
     status, recomendations = controller.exec_analysis(datapath=''.join(source_data), objective_column='ACCION',
-                             amode=FAST, metric='combined', deep_impact=4)
+                             amode=POC, metric='combined', deep_impact=3)
 
     controller.save_models(recomendations)
     controller.reconstruct_execution_tree(recomendations, metric='combined')
