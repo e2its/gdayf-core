a2c8fdf87b77b3b319d04910e08690a9e3ae288c
Jose L. Sanchez del Coso <e2itses@gmail.com>
2017-09-08 08:40:40 +0000

    Executable beta state branch 0.1.2-mrazul 08/09/2017 10:38 Executed: Test of K-means and autoencoder functionality
    State:
    Control Commit before ubuntu installation on MSI laptop
    
    Pending: 
    XGBoost (Pending ubuntu platform)
    Advanced knowledge, Test and Improvements. (Pending ubuntu platform)
    Optimizer Rules -> (Pending ubuntu platform)
    
    Sprint 4 (21/09) 3 weeks
    Include AutoEncoders Analysis (unsupervised) 
    Include K-means Clustering Analysis (unsupervised)
    
    Pending Sprint Task backlog:)
        Confident thresholds on predictions (Non prioritary)
        Advanced analysis over one selected model (checkpoint param) (Non prioritary)

--- src/gdayf/conf/loadconfig.py
+++ src/gdayf/conf/loadconfig.py
@@ -13,7 +13,7 @@ class LoadConfig(object):
     _configfile = None
 
     ## Constructor
-    def __init__(self, configfile=r'D:\e2its-dayf.svn\gdayf\branches\0.1.1-mrazul\src\gdayf\conf\config.json'):
+    def __init__(self, configfile=r'D:\e2its-dayf.svn\gdayf\branches\0.1.2-mrazul\src\gdayf\conf\config.json'):
         # @var _config protected member variable to store config parameters
         self._config = None
         # @var _configfile protected member variable to store configfile path
@@ -48,7 +48,7 @@ class LoadLabels(object):
     _configfile = None
 
     ## Constructor
-    def __init__(self, lang='en', configfile=r'D:\e2its-dayf.svn\gdayf\branches\0.1.1-mrazul\src\gdayf\conf\labels.json'):
+    def __init__(self, lang='en', configfile=r'D:\e2its-dayf.svn\gdayf\branches\0.1.2-mrazul\src\gdayf\conf\labels.json'):
         # @var _config protected member variable to store config parameters
         self._config = None
         # @var _configfile protected member variable to store configfile path
