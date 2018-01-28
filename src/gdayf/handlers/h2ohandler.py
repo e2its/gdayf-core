5f6acfcdb032b0d212ca9a13361573d8f480daef
Jose L. Sanchez del Coso <e2itses@gmail.com>
2018-01-28 20:05:23 +0000

    * Bux Fixed on log_info: Reuse H2O training DataFrame
    Sprint 7: 18/02/2018 (Due Date)
    * UX interface mockups
    
    
    Product backlog
    * Include Decision Tree conditions OrderedDict()
    * Multi-engine dynamical class loader
    * Cluster multi-node-execution
    * External export to PMML (not possible in this time only for mllib)
    * Keras-Tensorflow Integration
    * Confident thresholds on predictions (Non prioritary)
    * e2its web site
    * Synchronous to asynchronous behaviour through RabbitMQ

--- src/gdayf/handlers/h2ohandler.py
+++ src/gdayf/handlers/h2ohandler.py
@@ -797,7 +797,7 @@ class H2OHandler(object):
                                        ') validating_frame(' + str(valid_frame.nrows) + ')')
             else:
                 training_frame = get_frame('train_' + analysis_id + '_' + str(train_hash_value))
-                self._logging.log_exec(analysis_id, self._h2o_session.session_id, self._labels["getting_from_h2o"],
+                self._logging.log_info(analysis_id, self._h2o_session.session_id, self._labels["getting_from_h2o"],
                                        'training_frame(' + str(training_frame.nrows) + ')')
             if "test_frame" in kwargs.keys():
                 test_frame = get_frame('test_' + analysis_id + '_' + str(train_hash_value))
