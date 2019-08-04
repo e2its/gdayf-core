357d6501911703c1cc9997bfc93d633ec617d37e
Jose L. Sanchez del Coso <e2itses@gmail.com>
2019-08-04 12:13:39 +0200

    Update License headers to GPL3
     * This file is part of the gDayF AutoML Core Framework
     * distribution (https://github.com/e2its/gdayf-core).
     * Copyright (c) 2016-2019 Jose Luis Sanchez del Coso <e2its.es@gmail.com>.
     *
     * This program is free software: you can redistribute it and/or modify
     * it under the terms of the GNU General Public License as published by
     * the Free Software Foundation, version 3.
     *
     * This program is distributed in the hope that it will be useful, but
     * WITHOUT ANY WARRANTY; without even the implied warranty of
     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
     * General Public License for more details.
     *
     * You should have received a copy of the GNU General Public License
     * along with this program. If not, see <http://www.gnu.org/licenses/>.

--- src/gdayf/common/storagemetadata.py
+++ src/gdayf/common/storagemetadata.py
@@ -3,13 +3,22 @@
 # on list[OrderedDict] format
 
 '''
-Copyright (C) e2its - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ * This file is part of the gDayF AutoML Core Framework project
+ * distribution (https://github.com/e2its/gdayf-core).
+ * Copyright (c) 2016-2019 Jose Luis Sanchez del Coso <e2its.es@gmail.com>.
  *
- * This file is part of gDayF project.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
  *
- * Written by Jose L. Sanchez <e2its.es@gmail.com>, 2016-2019
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ** Written by Jose L. Sanchez <e2its.es@gmail.com>, 2016-2019
 '''
 
 from gdayf.common.utils import hash_key
